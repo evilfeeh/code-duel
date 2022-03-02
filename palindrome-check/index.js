44efbecf7c7bf74a77cf7a7bde0ea50bbc541ca7
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-01 22:58:47 -0300

    add caller on index.js

--- palindrome-check/index.js
+++ palindrome-check/index.js
@@ -1,4 +1,9 @@
-const nail = require('./nail')
+const Nail = require('./nail')
+const Native = require('./js-native')
 
-const palindromeCheck = new nail('ame o poema')
+
+const palindromeCheck = new Nail('ame o poema')
 palindromeCheck.invertString()
+
+const palindromeCheck = new Native('ame o poema')
+palindromeCheck.reverseString()
